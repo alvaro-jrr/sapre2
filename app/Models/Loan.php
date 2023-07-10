d8d24b17a0bec559f31b955938cef39e062f94a7
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-10 10:36:00 -0400

    Create relationship between user and loan

--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Loan extends Model
-{
-    use HasFactory;
+class Loan extends Model {
+	use HasFactory;
+
+	/**
+	 * Get the user that owns the loan.
+	 */
+	public function user(): BelongsTo {
+		return $this->belongsTo(User::class);
+	}
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -4,42 +4,43 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, HasRoles, Notifiable;
+class User extends Authenticatable {
+	use HasApiTokens, HasFactory, HasRoles, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $fillable = ["name", "email", "password"];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+	/**
+	 * The attributes that should be hidden for serialization.
+	 *
+	 * @var array<int, string>
+	 */
+	protected $hidden = ["password", "remember_token"];
+
+	/**
+	 * The attributes that should be cast.
+	 *
+	 * @var array<string, string>
+	 */
+	protected $casts = [
+		"email_verified_at" => "datetime",
+		"password" => "hashed",
+	];
+
+	/**
+	 * Get the loans requested by the user.
+	 */
+	public function loans(): HasMany {
+		return $this->hasMany(Loan::class);
+	}
 }
