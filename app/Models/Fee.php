0f33bb3847173e9795025f8c8dd6c25505e2b1c1
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-10 10:52:55 -0400

    Create relationship between fee and loan

--- app/Models/Fee.php
+++ app/Models/Fee.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Fee extends Model
-{
-    use HasFactory;
+class Fee extends Model {
+	use HasFactory;
+
+	/**
+	 * Get the loan paid by the fee.
+	 */
+	public function loan(): BelongsTo {
+		return $this->belongsTo(Loan::class);
+	}
 }
--- app/Models/Loan.php
+++ app/Models/Loan.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Loan extends Model {
 	use HasFactory;
@@ -23,5 +24,12 @@ class Loan extends Model {
 	 */
 	public function modality(): BelongsTo {
 		return $this->belongsTo(Modality::class);
+	}
+
+	/**
+	 * Get the fees created to pay the loan.
+	 */
+	public function fees(): HasMany {
+		return $this->hasMany(Fee::class);
 	}
 }
