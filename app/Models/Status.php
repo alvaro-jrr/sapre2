a9313d68c29af57103d36a2626cf440c24a37c40
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-12 11:27:39 -0400

    Remove timestamps for statuses

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -9,6 +9,8 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class Status extends Model {
 	use HasFactory;
 
+	public $timestamps = false;
+
 	protected $fillable = ["name", "slug"];
 
 	/**
