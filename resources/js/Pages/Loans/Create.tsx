ab8a06d50aa52bc8d918dc1f97bb045e6fc72f32
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-12 13:21:03 -0400

    Add create loan logic

--- resources/js/Pages/Loans/Create.tsx
+++ resources/js/Pages/Loans/Create.tsx
@@ -1,5 +1,5 @@
 import type { Modality, PageProps, User } from "@/types";
-import { Head, Link } from "@inertiajs/react";
+import { Head, Link, useForm } from "@inertiajs/react";
 import Header from "@/Components/Header";
 import RadioGroupField from "@/Components/RadioGroupField";
 import SelectField from "@/Components/SelectField";
@@ -7,12 +7,29 @@ import TextField from "@/Components/TextField";
 import { Button } from "@/Components/ui/button";
 import { Card, CardContent } from "@/Components/ui/card";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { FormEventHandler } from "react";
 
 export default function Create({
 	auth,
 	users,
 	modalities,
 }: PageProps<{ users: User[]; modalities: Modality[] }>) {
+	// Form controls
+	const { data, setData, post, processing, errors } = useForm({
+		client: "",
+		amount: "",
+		modality: String(modalities[0].id),
+		number_of_fees: "",
+		interest_rate: "",
+	});
+
+	// Submits the new loan
+	const submit: FormEventHandler = (e) => {
+		e.preventDefault();
+
+		post(route("loans.store"));
+	};
+
 	return (
 		<AuthenticatedLayout
 			user={auth.user}
@@ -27,19 +44,23 @@ export default function Create({
 
 			<Card>
 				<CardContent className="p-6">
-					<form className="space-y-6">
+					<form onSubmit={submit} className="space-y-6">
 						<div className="space-y-4">
 							<SelectField
 								id="client"
-								placeholder="Seleccione al cliente"
 								labelProps={{ children: "Cliente" }}
+								placeholder="Seleccione al cliente"
 								selectProps={{
 									name: "client",
+									required: true,
+									onValueChange: (value) =>
+										setData("client", value),
 								}}
 								options={users.map((user) => ({
 									label: `${user.name} (${user.email})`,
 									value: String(user.id),
 								}))}
+								errorMessage={errors.client}
 							/>
 
 							<TextField
@@ -51,11 +72,22 @@ export default function Create({
 									placeholder: "ej: 300.0",
 									autoComplete: "off",
 									step: "any",
+									value: data.amount,
+									onChange: (e) =>
+										setData("amount", e.target.value),
+									required: true,
 								}}
+								errorMessage={errors.amount}
 							/>
 
 							<RadioGroupField
 								labelProps={{ children: "Modalidades de Pago" }}
+								radioGroupProps={{
+									value: data.modality,
+									onValueChange: (value) =>
+										setData("modality", value),
+									required: true,
+								}}
 								options={modalities.map((modality) => ({
 									label: modality.name,
 									value: String(modality.id),
@@ -71,7 +103,14 @@ export default function Create({
 									placeholder: "ej: 1",
 									autoComplete: "off",
 									min: 1,
+									onChange: (e) =>
+										setData(
+											"number_of_fees",
+											e.target.value
+										),
+									required: true,
 								}}
+								errorMessage={errors.number_of_fees}
 							/>
 
 							<TextField
@@ -85,7 +124,14 @@ export default function Create({
 									step: "any",
 									min: 0,
 									max: 100,
+									onChange: (e) =>
+										setData(
+											"interest_rate",
+											e.target.value
+										),
+									required: true,
 								}}
+								errorMessage={errors.interest_rate}
 							/>
 						</div>
 
