7359903b99e37e5e40a23927c4365c7edee5c4e2
julian <julianlopezcastillo12@gmail.com>
2023-07-16 18:08:18 -0400

    feecontroller

--- app/Http/Controllers/FeeController.php
+++ app/Http/Controllers/FeeController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fee;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
 
 class FeeController extends Controller
 {
-    //
+    public function index(): Response {
+
+        return inertia::render("Fees/index", [
+            "fees" => Fee::all(["id", "name"]),
+        ]);
+
+
+    }
 }
