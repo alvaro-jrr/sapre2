eeea5f093736681ccc701c70ade7df3dbb5c7ce7
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2023-07-16 22:16:14 -0400

    Reset date filter correctly

--- resources/js/Pages/Loans/Partials/DataTableToolBar.tsx
+++ resources/js/Pages/Loans/Partials/DataTableToolBar.tsx
@@ -4,7 +4,9 @@ import { Table } from "@tanstack/react-table";
 import { Button } from "@/Components/ui/button";
 import { DataTableFacetedFilter } from "@/Components/DataTableFacetedFilter";
 import { Modality, Status } from "@/types";
-import { DataTableDateRangeFilter } from "@/Components/DataTableDateRangeFilter";
+import { useState } from "react";
+import { DateRange } from "react-day-picker";
+import { DatePickerWithRange } from "@/Components/DateRangePicker";
 
 interface DataTableToolbarProps<TData> {
 	table: Table<TData>;
@@ -17,6 +19,7 @@ export function DataTableToolbar<TData>({
 	statuses,
 	modalities,
 }: DataTableToolbarProps<TData>) {
+	const [dateRange, setDateRange] = useState<DateRange | undefined>();
 	const isFiltered = table.getState().columnFilters.length > 0;
 
 	if (
@@ -30,8 +33,14 @@ export function DataTableToolbar<TData>({
 	return (
 		<div className="flex flex-col justify-start gap-2 md:flex-row md:items-center">
 			{table.getColumn("created_at") ? (
-				<DataTableDateRangeFilter
-					column={table.getColumn("created_at")}
+				<DatePickerWithRange
+					label="Seleccione una fecha"
+					selected={dateRange}
+					onSelectDateRange={(value) => {
+						table.getColumn("created_at")?.setFilterValue(value);
+
+						setDateRange(value);
+					}}
 				/>
 			) : null}
 
@@ -61,7 +70,10 @@ export function DataTableToolbar<TData>({
 				{isFiltered && (
 					<Button
 						variant="ghost"
-						onClick={() => table.resetColumnFilters()}
+						onClick={() => {
+							setDateRange(undefined);
+							table.resetColumnFilters();
+						}}
 						className="h-8 w-fit px-2 lg:px-3"
 					>
 						Reiniciar
